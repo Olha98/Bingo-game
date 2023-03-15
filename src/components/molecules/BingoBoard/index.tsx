d78b48f43dd4c4529985c9e7acfb20e431c169fe
Olha Hrymchak <olhahrymchak@MacBook-Pro-Olha-2.local>
2023-03-15 21:32:23 +0100

    feat: adjust logic for winer

--- src/components/molecules/BingoBoard/index.tsx
+++ src/components/molecules/BingoBoard/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
 import * as S from './style';
 
@@ -17,9 +17,40 @@ export const BingoBoard: React.FC<{ board: any }> = ({ board }) => {
     }
   };
 
+  const checkWin = () => {
+    for (let i = 0; i < 5; i++) {
+      // filtering for rows
+      if (calledNumbers.filter(n => Math.floor(n / 5) === i).length === 5) {
+        setIsWin(true);
+        return;
+      }
 
+      // filtering for columns
+      if (calledNumbers.filter(n => n % 5 === i).length === 5) {
+        setIsWin(true);
+        return;
+      }
 
+      // filtering for diagonals from top-left to bottom-right
+      if (calledNumbers.filter(n => n % 6 === i).length === 5) {
+        console.log('here');
+        setIsWin(true);
+        return;
+      }
+      // filtering for diagonals from top-right to bottom-left
 
+      if (calledNumbers.filter(n => n % 4 === 0 && n !== 0 && n !== 24).length === 5) {
+        console.log('here');
+        setIsWin(true);
+        return;
+      }
+      setIsWin(false);
+    }
+  };
+  useEffect(() => {
+    checkWin();
+    console.log(isWin, 'isWin');
+  }, [calledNumbers, isWin]);
 
   return (
     <S.BingoBoardTable>
